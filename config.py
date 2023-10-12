e416ae735c2447b55a7beb4d793b4ad39ea106a0
nvaardian <123327658+nvaardian@users.noreply.github.com>
2023-10-12 17:08:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,9 @@
+
 import os
-from os import getenv
+
 from dotenv import load_dotenv
-from distutils.util import strtobool as sb
-from base64 import b64decode
 
-load_dotenv()
-
-
-
-DEVS = [
-  6302436328, #sy
-]
-class Config(object):
+load_dotenv("config.env" if os.path.isfile("config.env") else "sample_config.env")
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 API_ID = int(os.environ.get('API_ID'))
@@ -30,5 +22,3 @@ ARQ_API_URL = os.environ.get('ARQ_API_URL', 'https://arq.hamker.in')
 LOG_MENTIONS = os.environ.get('LOG_MENTIONS', 'True').lower() in ['true', '1']
 RSS_DELAY = int(os.environ.get('RSS_DELAY', 300))
 PM_PERMIT = os.environ.get('PM_PERMIT', 'True').lower() in ['true', '1']
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
-HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
