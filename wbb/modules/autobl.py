ec7f7da52e7d40bbba33a1f7bef7162ce48fe2da
Inopem Adika <49522880+npdkdev@users.noreply.github.com>
2023-11-08 21:52:36 +0700

    kontol jembot fajar v5

--- wbb/modules/autobl.py
+++ wbb/modules/autobl.py
@@ -1,4 +1,5 @@
 import re
+import traceback
 
 import emoji
 from pyrogram import filters, Client
@@ -15,29 +16,37 @@ chat_id = [-1001710412230,-1001629982867]
 
 @app.on_message(filters.text & filters.chat(-1001629982867) & ~filters.private, group=5)
 async def dk_validate_bl(c: Client, message: Message):
-    text = message.text or None
-    if not text:
-        return False
-    # remove emojis first
-    text_parts = []
-    current_text = ""
-    for char in text:
-        if emoji.emoji_count(char) == 0:
-            current_text += char
-        else:
-            if current_text:
-                text_parts.append(current_text)
-            current_text = ""
-    if current_text:
-        text_parts.append(current_text)
-    text_2 = ""
-    for part in text_parts:
-        text_2 += part
+    try:
+        text = message.text or None
+        print(text)
+        if not text:
+            return False
+        # remove emojis first
+        text_parts = []
+        current_text = ""
+        for char in text:
+            if emoji.emoji_count(char) == 0:
+                current_text += char
+            else:
+                if current_text:
+                    text_parts.append(current_text)
+                current_text = ""
+        if current_text:
+            text_parts.append(current_text)
+        text_2 = ""
+        for part in text_parts:
+            text_2 += part
+        print(text_2)
 
-    # remove numbers from text
-    text_3 = re.sub(r'[0-9]+', '', text_2)
-    #check if text is uppercase
-    # text_4 = text_3.isupper()
-    # check if text uppercase is more than lowercase
-    text_5 = sum(1 for c in text_3 if c.isupper()) > sum(1 for c in text_3 if c.islower())
-    return text_5
+        # remove numbers from text
+        text_3 = re.sub(r'[0-9]+', '', text_2)
+        print(text_3)
+        #check if text is uppercase
+        # text_4 = text_3.isupper()
+        # check if text uppercase is more than lowercase
+        text_5 = sum(1 for c in text_3 if c.isupper()) > sum(1 for c in text_3 if c.islower())
+        print(text_5)
+        return text_5
+    except Exception as e:
+        print(traceback.format_exc())
+        return False
