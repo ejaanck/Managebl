1b4ed9d4fd6fb9f884f491dcd24b1502c33abfa6
Fajarad03 <144516526+Fajarad03@users.noreply.github.com>
2024-01-19 21:37:52 +0700

    Update blacklist.py

--- wbb/modules/blacklist.py
+++ wbb/modules/blacklist.py
@@ -117,7 +117,7 @@ async def del_filter(_, message):
     await message.reply_text("**No such blacklist filter.**")
 
 
-@app.on_message(filters.text & filters.chat(chat_id) & ~filters.private, group=blacklist_filters_group)
+@app.on_message(filters.text & ~filters.private, group=blacklist_filters_group)
 @capture_err
 async def blacklist_filters_re(_, message):
     text = message.text.lower().strip()
